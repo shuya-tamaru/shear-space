18271c1a9b0e3d305fd2a4aad106acc90072ae4d
shuya-tamaru <tamaru1013@gmail.com>
2020-03-29 21:56:10 +0900

    ログインボタン実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_113325) do
+ActiveRecord::Schema.define(version: 2020_03_28_133232) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 2020_03_05_113325) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "image"
@@ -57,4 +66,5 @@ ActiveRecord::Schema.define(version: 2020_03_05_113325) do
   add_foreign_key "images", "posts"
   add_foreign_key "posts", "categories"
   add_foreign_key "posts", "users"
+  add_foreign_key "sns_credentials", "users"
 end
