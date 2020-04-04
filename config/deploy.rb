e0d0af183800c84b18920c5066b552218ee59ca3
shuya-tamaru <tamaru1013@gmail.com>
2020-04-05 00:37:57 +0900

    S3記述修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repo_url,  'git@github.com:shuya-tamaru/shear-space.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
@@ -35,7 +35,7 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-  
+
   desc 'upload master.key'
   task :upload do
     on roles(:app) do |host|
