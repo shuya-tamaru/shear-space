360f248e408213125bdd417fb150e5341e1b2eac
shuya-tamaru <tamaru1013@gmail.com>
2020-04-25 20:54:42 +0900

    unicorn修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,6 +6,8 @@ worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
 working_directory "#{app_path}/current"
+working_directory rails_root
+
 
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
