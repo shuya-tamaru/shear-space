1ba0e9d75c841a6b0446dc41eb72b6217823befe
shuya-tamaru <tamaru1013@gmail.com>
2020-04-27 19:59:25 +0900

    errorの確認

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  protect_from_forgery except: :facebook
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
