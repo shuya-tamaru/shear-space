38d68ea8569fdcf0712556e9c0222b17491aaf9a
shuya-tamaru <tamaru1013@gmail.com>
2020-05-01 20:44:16 +0900

    unicorn.rb修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-  omniauth_callbacks: 'users/omniauth_callbacks',
-  registrations: 'users/registrations'
-}
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   root to: 'posts#index'
   resources :users, only: [:index, :show]
   resources :privasies, only: [:index]
